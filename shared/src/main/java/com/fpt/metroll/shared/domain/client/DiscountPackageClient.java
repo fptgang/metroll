87590da72a1f51135550efa9cc1240690888b31a
Anh Duy <143704010+anhhd45@users.noreply.github.com>
2025-06-10 14:54:29 +0700

    Add DiscountPackageClient

--- shared/src/main/java/com/fpt/metroll/shared/domain/client/DiscountPackageClient.java
+++ shared/src/main/java/com/fpt/metroll/shared/domain/client/DiscountPackageClient.java
@@ -1,13 +1,14 @@
 package com.fpt.metroll.shared.domain.client;
 
 import com.fpt.metroll.shared.domain.dto.account.AccountDto;
+import com.fpt.metroll.shared.domain.dto.discount.DiscountPackageDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "account-service")
-public interface AccountClient {
+public interface DiscountPackageClient {
 
-    @GetMapping("/account/accounts/{accountId}")
-    AccountDto getAccount(@PathVariable("accountId") String accountId);
+    @GetMapping("/account/discount-packages/{packageId}")
+    DiscountPackageDto getDiscountPackage(@PathVariable("packageId") String packageId);
 }
