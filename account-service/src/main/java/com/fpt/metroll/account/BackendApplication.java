ccccdccbe6bfca582d5e05a6f2e2e8c8b862995c
Anh Duy <143704010+anhhd45@users.noreply.github.com>
2025-07-23 23:09:49 +0700

    Add voucher expiry scheduler

--- account-service/src/main/java/com/fpt/metroll/account/BackendApplication.java
+++ account-service/src/main/java/com/fpt/metroll/account/BackendApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.time.ZoneOffset;
 import java.util.TimeZone;
@@ -11,6 +12,7 @@ import java.util.TimeZone;
 @SpringBootApplication(scanBasePackages = {"com.fpt.metroll"})
 @EnableFeignClients(basePackages = {"com.fpt.metroll"})
 @EnableCaching
+@EnableScheduling
 public class BackendApplication {
 
     public static void main(String[] args) {
--- account-service/src/main/java/com/fpt/metroll/account/repository/VoucherRepository.java
+++ account-service/src/main/java/com/fpt/metroll/account/repository/VoucherRepository.java
@@ -1,9 +1,11 @@
 package com.fpt.metroll.account.repository;
 
 import com.fpt.metroll.account.document.Voucher;
+import com.fpt.metroll.shared.domain.enums.VoucherStatus;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,4 +14,6 @@ public interface VoucherRepository extends MongoRepository<Voucher, String> {
     boolean existsByCode(String code);
 
     Optional<Voucher> findByCode(String code);
+    
+    List<Voucher> findByStatusAndValidUntilBefore(VoucherStatus status, Instant expiredDate);
 }
